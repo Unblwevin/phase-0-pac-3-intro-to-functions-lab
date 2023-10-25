9bacb4d5948aa97306da16e99550dff1b7abf7af
KevinWilliams <kw119919@gmail.com>
2023-10-25 14:31:49 -0700

    done

---
+++ index.js
@@ -0,0 +1,33 @@
+const expect = require("expect");
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    console.log(string);
+    console.log(string.toUpperCase());
+
+    console.log(string === string.toUpperCase());
+  }
+
+  function logWhisper(string) {
+    console.log(string);
+    console.log(string.toLowerCase());
+
+    console.log(string === string.toLowerCase());
+  }
+
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+     return"I can\'t hear you!"
+        else if (string === 'HELLO')
+      return "YES INDEED!"
+       else (string === "Let's have dinner together!")
+       return "I would love to!"
+  }
+  
